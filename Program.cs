36f36e1038d2117cbd3d2a5012d5aae8532db15f
DESKTOP-G085NET\csi23-nrojlla <csi23-nrojlla@colegioaltair.net>
2024-02-12 14:27:00 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Agregar_y_restar_dias_ej7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una fecha formato (DD/MM/YYYY)");
+            DateTime fechaEscogida = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese un numero de dias a sumar");
+            int numeroASumar = Convert.ToInt32(Console.ReadLine());
+
+            
+            DateTime sumarFechas = fechaEscogida.AddDays(numeroASumar);
+            Console.WriteLine("Dias sumados");
+            Console.WriteLine(sumarFechas.Date);
+
+            DateTime restarFechas = fechaEscogida.AddDays(-numeroASumar);
+            Console.WriteLine("Dias restados");
+            Console.WriteLine(restarFechas.Date);
+
+
+        }
+    }
+}
